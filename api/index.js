d46e195c9f558ef2733b858367236c8cb73d287c
Aarti-Sharma21 <aarti252002156@gmail.com>
2023-12-13 11:58:13 +0530

    middleware and a function to handle posssible errors

--- api/index.js
+++ api/index.js
@@ -29,3 +29,18 @@ app.use('/api/user' , userRouter );
 // sign up api route
 
 app.use('/api/auth' , authRouter);
+
+
+//middleware for error
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json(
+        {
+            success : false,
+            statusCode,
+            message,
+        }
+    );
+});
